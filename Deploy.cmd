0fb09c84cc8c13f576fd0fca0613256e7c5a66fb
a.antenore <a.antenore@reply.it>
2022-12-13 15:03:03 +0100

    restart port forward on deploy

--- Deploy.cmd
+++ Deploy.cmd
@@ -2,3 +2,4 @@ cmd /c StartMinikube
 kubectl rollout restart deployment/fe-app
 kubectl apply -f ../cluster-config/fe-deployment.yaml
 REM kubectl apply -f ../cluster-config/fe-service.yaml
+cmd /c Start.cmd
