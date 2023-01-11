a0b6268b952fb52f6e3a2f585f6be4fa30101c5e
a.antenore <a.antenore@reply.it>
2023-01-11 10:51:43 +0100

    sdk example with guest token

--- src/components/SdkExample/SdkExample.js
+++ src/components/SdkExample/SdkExample.js
@@ -5,21 +5,22 @@ import axios from 'axios';
 import { embedDashboard } from "@superset-ui/embedded-sdk";
 
 function SdkExample() {
-  const SUPERSET_CONTAINER_ID = "superset-container";
+  const SUPERSET_CONTAINER_ID = "superset-container"
+  const id = "11593394-f71f-4e05-a59e-c83174bb02be"
+  const host = 'http://localhost:32001'
 
-  let token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjp7Imxhc3RfbmFtZSI6IlJvc3NpIiwidXNlcm5hbWUiOiJtcm9zc2kiLCJmaXJzdF9uYW1lIjoiTWFyaW8ifSwicmVzb3VyY2VzIjpbeyJpZCI6IjExNTkzMzk0LWY3MWYtNGUwNS1hNTllLWM4MzE3NGJiMDJiZSIsInR5cGUiOiJkYXNoYm9hcmQifV0sInJsc19ydWxlcyI6W10sImlhdCI6MTY3MzM4MjA1NC42NzQ2NDQyLCJleHAiOjE2NzMzODIzNTQuNjc0NjQ0MiwiYXVkIjoiaHR0cDovL3N1cGVyc2V0OjgwODgvIiwidHlwZSI6Imd1ZXN0In0.HYONgV8_sCTyxB_keU_K-tTRh6hmqMZzqOatXWg8nIc'
-  
-  let embedDashboardOnClick = () => embedDashboard({
-      id: "11593394-f71f-4e05-a59e-c83174bb02be",
+  useEffect(() => {
+    embedDashboard({
+      id: id,
       supersetDomain: "http://localhost:32000",
       mountPoint: document.getElementById(SUPERSET_CONTAINER_ID),
-      fetchGuestToken: () => token,
+      fetchGuestToken: () => axios.get(host + '/guest_token/' + id).then(response => response.data.token),
       dashboardUiConfig: { hideTitle: true }
-  });
+    })
+  }, [])
 
   return <>
   <div id = {SUPERSET_CONTAINER_ID}></div>
-  <button onClick= {embedDashboardOnClick}>Embed</button>
   </>
 }
 
